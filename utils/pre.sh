519556c5b6b73fa144643c0f511d43ad14a59f77
Justen Stall <justenstall@gmail.com>
2023-07-24 14:55:34 -0400

    format pre.sh

--- utils/pre.sh
+++ utils/pre.sh
@@ -33,23 +33,23 @@ yellow_excl="${yellow}!${normal}"
 red_x="${red}✗${normal}"
 
 success() {
-	echo "${green_check} ${1}"
+  echo "${green_check} ${1}"
 }
 
 warning() {
-	echo "${yellow_excl} ${1}"
+  echo "${yellow_excl} ${1}"
 }
 
 warning_yellow() {
-	echo "${yellow_excl}${yellow} ${1}${normal}"
+  echo "${yellow_excl}${yellow} ${1}${normal}"
 }
 
 failure() {
-	echo "${red_x} ${1}"
+  echo "${red_x} ${1}"
 }
 
 failure_red() {
-	echo "${red_x}${red} ${1}${normal}"
+  echo "${red_x}${red} ${1}${normal}"
 }
 
 ############################################################
@@ -57,64 +57,64 @@ failure_red() {
 ############################################################
 
 sanitize() {
-	# https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html#:~:text=%24%7Bparameter//pattern/string%7D
-	result="$1"
-	result="${result//$bold/}"
-	result="${result//$normal/}"
-	result="${result//$red/}"
-	result="${result//$green/}"
-	result="${result//$yellow/}"
-	echo "${result//$blue/}"
+  # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html#:~:text=%24%7Bparameter//pattern/string%7D
+  result="$1"
+  result="${result//$bold/}"
+  result="${result//$normal/}"
+  result="${result//$red/}"
+  result="${result//$green/}"
+  result="${result//$yellow/}"
+  echo "${result//$blue/}"
 }
 
 box_out() {
-	# Initialize vars
-	local s=("$@") b w
+  # Initialize vars
+  local s=("$@") b w
 
-	# Iterate over arguments to find longest line and its length
-	for l in "${s[@]}"; do
-		# Sanitize line so format characters are not counted
-		plain=$(sanitize "$l")
-		((w < ${#plain})) && {
-			b="$plain"
-			w="${#plain}"
-		}
-	done
-	# Output top border by replacing characters in longest string with dash
-	echo ".-${b//?/-}-."
-	for l in "${s[@]}"; do
-		# Calculate padding size without format characters
-		plain=$(sanitize "$l")
-		padding=$((w - ${#plain} + 1))
-		printf '| %s%*s|\n' "$l" "-$padding" " "
-	done
-	# Output bottom border using same approach as top border
-	echo "'-${b//?/-}-'"
+  # Iterate over arguments to find longest line and its length
+  for l in "${s[@]}"; do
+    # Sanitize line so format characters are not counted
+    plain=$(sanitize "$l")
+    ((w < ${#plain})) && {
+      b="$plain"
+      w="${#plain}"
+    }
+  done
+  # Output top border by replacing characters in longest string with dash
+  echo ".-${b//?/-}-."
+  for l in "${s[@]}"; do
+    # Calculate padding size without format characters
+    plain=$(sanitize "$l")
+    padding=$((w - ${#plain} + 1))
+    printf '| %s%*s|\n' "$l" "-$padding" " "
+  done
+  # Output bottom border using same approach as top border
+  echo "'-${b//?/-}-'"
 }
 
 double_box_out() {
-	# Initialize vars
-	local s=("$@") b w
+  # Initialize vars
+  local s=("$@") b w
 
-	# Iterate over arguments to find longest line and its length
-	for l in "${s[@]}"; do
-		# Sanitize line so format characters are not counted
-		plain=$(sanitize "$l")
-		((w < ${#plain})) && {
-			b="$plain"
-			w="${#plain}"
-		}
-	done
-	# Output top border by replacing characters in longest string with dash
-	echo "╔=${b//?/=}=╗"
-	for l in "${s[@]}"; do
-		# Calculate padding size without format characters
-		plain=$(sanitize "$l")
-		padding=$((w - ${#plain} + 1))
-		printf '║ %s%*s║\n' "$l" "-$padding" " "
-	done
-	# Output bottom border using same approach as top border
-	echo "╚=${b//?/=}=╝"
+  # Iterate over arguments to find longest line and its length
+  for l in "${s[@]}"; do
+    # Sanitize line so format characters are not counted
+    plain=$(sanitize "$l")
+    ((w < ${#plain})) && {
+      b="$plain"
+      w="${#plain}"
+    }
+  done
+  # Output top border by replacing characters in longest string with dash
+  echo "╔=${b//?/=}=╗"
+  for l in "${s[@]}"; do
+    # Calculate padding size without format characters
+    plain=$(sanitize "$l")
+    padding=$((w - ${#plain} + 1))
+    printf '║ %s%*s║\n' "$l" "-$padding" " "
+  done
+  # Output bottom border using same approach as top border
+  echo "╚=${b//?/=}=╝"
 }
 
 ############################################################
@@ -124,73 +124,73 @@ double_box_out() {
 # System Check: Set XDG dirs and WSL variable
 IS_WSL=false
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-	XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-	# CHECKS IF RUNNING IN WSL
-	# https://github.com/microsoft/WSL/issues/423#issuecomment-221627364
-	if [ -f /proc/version ] && grep -qi 'microsoft\|wsl' /proc/version; then
-		# success "Detected Windows Subsystem for Linux (WSL) installation"
-		IS_WSL=true
-	fi
+  XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+  XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+  # CHECKS IF RUNNING IN WSL
+  # https://github.com/microsoft/WSL/issues/423#issuecomment-221627364
+  if [ -f /proc/version ] && grep -qi 'microsoft\|wsl' /proc/version; then
+    # success "Detected Windows Subsystem for Linux (WSL) installation"
+    IS_WSL=true
+  fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/Library/Caches"}"
-	XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/Library/Application Support}"
+  XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/Library/Caches"}"
+  XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/Library/Application Support}"
 else
-	XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-	XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+  XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+  XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 fi
 
 LOG_FILE="$XDG_CACHE_HOME/act3/login/logs.txt"
 
 redact() {
-	echo "${*/$TOKEN/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*}"
+  echo "${*/$TOKEN/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*}"
 }
 
 log_echo() {
-	redact "$@" >>"$LOG_FILE"
+  redact "$@" >>"$LOG_FILE"
 }
 
 log_eval() {
-	log_echo ""
-	log_echo "\$ $*"
-	echo "Args after log_echo: $*" >>"$LOG_FILE"
-	"$@" >>"$LOG_FILE" 2>&1
+  log_echo ""
+  log_echo "\$ $*"
+  echo "Args after log_echo: $*" >>"$LOG_FILE"
+  "$@" >>"$LOG_FILE" 2>&1
 }
 
 log_out() {
-	log_echo ""
-	log_echo "\$ $*"
-	echo "Args after log_echo: $*" >>"$LOG_FILE"
-	output=$("$@" 2>&1)
-	exit_code=$?
-  	log_echo "$output"
-  	echo "$output"
-  	return "$exit_code"
+  log_echo ""
+  log_echo "\$ $*"
+  echo "Args after log_echo: $*" >>"$LOG_FILE"
+  output=$("$@" 2>&1)
+  exit_code=$?
+  log_echo "$output"
+  echo "$output"
+  return "$exit_code"
 }
 
 create_log_file() {
-	[ -f "$LOG_FILE" ] && rm "$LOG_FILE"                       # remove existing log file (if exists)
-	mkdir -p "$XDG_CACHE_HOME/act3/login" && touch "$LOG_FILE" # create new log file
-	log_echo "ACT3 Login Logs: $(date "+%D %T")"
-	success "Created log file ${bold}${LOG_FILE}${normal}"
+  [ -f "$LOG_FILE" ] && rm "$LOG_FILE"                       # remove existing log file (if exists)
+  mkdir -p "$XDG_CACHE_HOME/act3/login" && touch "$LOG_FILE" # create new log file
+  log_echo "ACT3 Login Logs: $(date "+%D %T")"
+  success "Created log file ${bold}${LOG_FILE}${normal}"
 }
 
 append_to_file() {
-	mkdir -p "$(dirname "$1")" && touch -a "$1" # ensure file exists
-	echo "$2" >>"$1"                            # concatenate to file
+  mkdir -p "$(dirname "$1")" && touch -a "$1" # ensure file exists
+  echo "$2" >>"$1"                            # concatenate to file
 }
 
 check_logs() {
-	box_out "Check log file: ${bold}${LOG_FILE}${normal}" \
-		"Troubleshooting: ${bold}https://github.com/act3-ace/aceup/blob/main/docs/troubleshooting-faq.md${normal}" \
-		"Create Support Ticket: ${bold}https://git.act3-ace.com/ace/aceup/-/issues/new?issuable_template=Support%20Ticket${normal}"
+  box_out "Check log file: ${bold}${LOG_FILE}${normal}" \
+    "Troubleshooting: ${bold}https://github.com/act3-ace/aceup/blob/main/docs/troubleshooting-faq.md${normal}" \
+    "Create Support Ticket: ${bold}https://git.act3-ace.com/ace/aceup/-/issues/new?issuable_template=Support%20Ticket${normal}"
 }
 
 abort() {
-	echo "Aborting."
-	echo
-	check_logs
-	exit 1
+  echo "Aborting."
+  echo
+  check_logs
+  exit 1
 }
 
 ############################################################
